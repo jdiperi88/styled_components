e62dd865bce2adbd83c8677476244a8950493e4f
jdiperi88 <jdiperi88@yahoo.com>
2019-03-17 11:03:44 -0400

    created global styles

--- src/App.js
+++ src/App.js
@@ -1,30 +1,97 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import GlobalStyle from "./Global";
+const sizes = {
+  small: 400,
+  med: 960,
+  large: 1140
+};
+
+const above = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (min-width: ${sizes[label]}px) {
+      ${css(...args)}
+    }
+  `;
+  return acc;
+}, {});
+
+const below = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (min-width: ${sizes[label]}px) {
+      ${css(...args)}
+    }
+  `;
+  return acc;
+}, {});
+
+const Fake = ({ className }) => (
+  <div className={className}>
+    <h2>I am a fake component</h2>
+  </div>
+);
+
+//CSS helper
+const fixedTop = css`
+  position: fixed;
+  top: ${({ top }) => top + "px"};
+  left: 0;
+`;
 
 const Heading = styled.h1`
   font-size: 10rem;
+  ${above.med`
+  color:blue`}
 `;
-
+const color = "indigo";
 const Button = styled.button`
-  background: indigo;
+  background: ${color};
   padding: 5px 10px;
   border-radius: 4px;
   color: white;
   font-size: 2rem;
   border: none;
-  ${props => props.type === "cancel" && "background: tomato;"}
+  background: tomato;
+`;
+
+const CancelButton = styled(Button)`
+  background: purple;
+  ${fixedTop}
+`;
+
+const AppWrapper = styled.div`
+  header {
+    background: teal;
+    &:hover {
+      background: red;
+    }
+  }
+  ${Button} {
+    margin-bottom: 200px;
+  }
+`;
+
+const DoubleFake = styled(Fake)`
+  h2 {
+    color: red;
+  }
 `;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Heading>Header</Heading>
-        <Button>Save</Button>
-        <Button type="cancel">Cancel</Button>
-      </div>
+      <AppWrapper>
+        <header>
+          <Heading>Header</Heading>
+          <DoubleFake />
+          <Button>Save</Button>
+          <CancelButton top={200} type="cancel">
+            Cancel
+          </CancelButton>
+        </header>
+        <GlobalStyle />
+      </AppWrapper>
     );
   }
 }
